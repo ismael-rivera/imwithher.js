470d360fab83e8b1c46dee0a8e103e1ff1aa4784
Ismael Ishy Rivera <coralblue79@gmail.com>
2016-12-16 17:59:20 -0200

    character module begun

--- assets/js/gdata.js
+++ assets/js/gdata.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
 
 // do jQuery
-$.getJSON("assets/data/data.json", function(result){	        
+	$.getJSON("assets/data/storygrid.json", function(result){	        
 
-        $.each(result, function(i, row){
-        	console.log(row);
-        	console.log(row.inciting_incident);
+	        $.each(result, function(i, row){
+	        	console.log(row);
+	        	console.log(row.inciting_incident);
 
-        	var out =	 "<tr>";
-				out +=      "<td>" + row.act.number + "</td>";
-				out +=      "<td>" + row.chapter.number + "</td>";
-				out +=      "<td>" + row.inciting_incident + "</td>";
-				out +=      "<td>" + row.complication + "</td>";
-				out +=      "<td>" + row.crisis + "</td>";
-				out +=      "<td>" + row.climax + "</td>";
-				out +=      "<td>" + row.resolution + "</td>";
-				out +=   "</tr>";
+	        	var out =	 "<tr>";
+					out +=      "<td>" + row.act.number + "</td>";
+					out +=      "<td>" + row.chapter.number + "</td>";
+					out +=      "<td>" + row.inciting_incident + "</td>";
+					out +=      "<td>" + row.complication + "</td>";
+					out +=      "<td>" + row.crisis + "</td>";
+					out +=      "<td>" + row.climax + "</td>";
+					out +=      "<td>" + row.resolution + "</td>";
+					out +=   "</tr>";
 
-            $("#storygrid tbody").append(out);
-        });
-    });
+	            $("#storygrid tbody").append(out);
+	        });
+	});
 
+	
 
 })
 
